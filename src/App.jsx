8c6278454355e1116709ce895689447c4283563e
Cristina Ferrer <krishee107@gmail.com>
2023-04-19 10:20:22 +0200

    Añadir tareas mediante el form

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import Navbar from "./components/Navbar/Navbar"
 import './App.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddForm from "./components/AddForm/AddForm";
 
 const App = () => {
-  const [task, setTask] = useState([])
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => {
+    console.log("ENTRA")
+    setTasks([...tasks, newTask]);
+  }
+
+  useEffect(() => {
+    console.log(tasks)
+  }, [tasks])
 
   return (
     <div className="todo-app">
       <h1>#todo</h1>
       <div className="todo-app_content">
         <Navbar />
-        <AddForm />
+        <AddForm addTask={addTask} />
+
+        <div className="taskList">
+          {tasks.map((task, index) => (
+            <div key={index}>
+              <span>{task.title}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -1,14 +1,37 @@
+import { useState } from 'react';
 import './AddForm.css';
 
-const AddForm = () => {
+const AddForm = (props) => {
+  const [task, setTask] = useState({
+    title: '',
+    status: 'active',
+  });
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setTask({
+      ...task,
+      title: value,
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.addTask(task);
+    setTask({
+      title: '',
+      status: 'active',
+    });
+  }
+
   return (
     <div className="addForm">
-      <form>
-        <input type="text" name="task" id="task" placeholder="add details" />
-        <button>Add</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="task" id="task" placeholder="add details" value={task.title} onChange={handleInputChange} />
+        <button type='submit'>Add</button>
       </form>
     </div>
   )
 }
 
-export default AddForm
+export default AddForm;
